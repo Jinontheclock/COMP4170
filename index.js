1421dfde9868c193c8d1f0cdb1b735acb5f8cc27
emmpup <emisola@my.bcit.ca>
2026-03-11 08:29:04 -0700

    Set up server code

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
