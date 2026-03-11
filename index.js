83f80bead698b47627160cd35d368b0c71898ae0
emmpup <emisola@my.bcit.ca>
2026-03-11 08:40:39 -0700

    Add database setup

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+db.connect();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
