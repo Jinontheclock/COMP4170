f3c15e24d61f959e70584948c3dc954d1592cf3d
Belinda To <bto10@my.bcit.ca>
2026-03-14 12:51:44 -0700

    adding results pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pg from "pg";
+import animals from "./animals.js";
 
 const app = express();
 const port = 3000;
@@ -12,13 +13,19 @@ const db = new pg.Client({
   password: process.env.DB_PASSWORD,
 });
 
-db.connect();
+db.connect().catch(err => console.error("DB connection error:", err.message));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
+});
+
+app.get("/result", (req, res) => {
+  const result = animals[req.query.type] || animals["graphic_designer"];
+  res.render("result", { result });
 });
 
 app.listen(port, () => {
